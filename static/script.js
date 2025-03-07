1ce1edb46ed24e8df2d1b72ecfbfd1d83cd68fa0
Amodh1257 <amodh1257@gmail.com>
2025-03-06 20:43:33 -0500

    Added a simple flask template to work with

---
+++ static/script.js
@@ -0,0 +1,6 @@
+// Log "Hello World" to the console
+var message = "Hello World";
+console.log("Hello World");
+var heading = document.createElement('h1'); // Create a new Heading-1 element
+heading.textContent = message; // Set the content of the heading
+document.body.appendChild(heading); // Add the heading to the body
---
+++ static/script.ts
@@ -0,0 +1,7 @@
+// Log "Hello World" to the console
+let message: string = "Hello World";
+console.log("Hello World")
+
+let heading: HTMLElement = document.createElement('h1');	// Create a new Heading-1 element
+heading.textContent = message;				// Set the content of the heading
+document.body.appendChild(heading);			// Add the heading to the body
